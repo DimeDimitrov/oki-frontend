e7bb38fea348ce566fbaf48ef6b772c694b69634
DimeDimitrov <129460051+DimeDimitrov@users.noreply.github.com>
2024-01-24 20:21:59 +0100

    Add google search index attemp #1

--- app.vue
+++ app.vue
@@ -9,12 +9,12 @@
 </template>
 
 <script setup lang="ts">
-
 useHead({
   title: 'Статистики за возачки испити',
   meta: [
     { name: 'keywords', content: 'возачки испити, испитен центар, статистики, statistiki, Македонија, ispiten centar, vozacki ispiti, rezultati, vozacki testovi, vozacki ispit, rezultati, pass rate' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: "google-site-verification", content: "n0uyPcWfyO-8mPWUH9YVrUttHKcx54l1kYi_VZjfHLc"}
   ],
 })
 useSeoMeta({
